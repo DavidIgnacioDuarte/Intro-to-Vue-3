d45eaef97b9d47046dcc5c5003e10496098b1a42
duarted <duarted@aaip.gob.ar>
2022-07-12 09:40:49 -0300

    Lección 3 fin

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            link: 'https://articulo.mercadolibre.com.ar/MLA-896232226-pack-de-medias-con-dibujos-6-unidades-_JM#position=3&search_layout=stack&type=item&tracking_id=e35c900c-7db9-4472-a59b-513b1d428bef'
         }
     }
 })
