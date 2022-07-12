1a4beee5ee00f65f0e96589f4c89ac77204a634f
duarted <duarted@aaip.gob.ar>
2022-07-12 10:12:29 -0300

    List Rendering realizado

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: [{t:'S', cm:'35'}, {t:'M', cm:'40'}, {t:'L', cm:'45'}, {t:'XL', cm:'50'} ]
         }
     }
 })
